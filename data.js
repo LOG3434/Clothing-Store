5784132faaca742817d5d68c8499f8f862482782
LOG3434 <logeshasai@gmail.com>
2025-03-17 13:25:30 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,88 @@
+let shopItemsData = [{
+    id:"ggtuyiutyi",
+    name: "Casual Shirt",
+    Price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing. ",
+    img:"images/img-1.jpg"
+},
+{
+    id:"okhliujls",
+    name: "Office Shirt",
+    Price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing. ",
+    img:"images/img-2.jpg"
+},
+    
+    {
+        id:"rtyuufuu",
+        name: "T Shirt",
+        Price: 25,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing. ",
+        img:"images/img-3.jpg"
+    },
+        {
+            id:"wsrfygfghg",
+            name: "Mens Suit",
+            Price: 300,
+            desc: "Lorem ipsum dolor sit amet consectetur adipisicing. ",
+            img:"images/img-4.jpg"
+        }, 
+                
+                {
+                  id: "thiecbawdjksadjk",
+                  name: "Mens Tie",
+                  Price: 25,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-5.png",
+                },
+                {
+                  id: "iuertrywebncdjksadjk",
+                  name: "Casual shoes",
+                  Price: 200,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-6.png",
+                },
+                {
+                  id: "thierytbvcbvzdhadjk",
+                  name: "black suit",
+                  Price: 450,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-7.png",
+                },
+                {
+                  id: "trfoiwfcnbcawdjksadjk",
+                  name: "polo shirt",
+                  Price: 45,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-8.png",
+                },
+                {
+                  id: "cbvxbcvsceldk",
+                  name: "denim shirt",
+                  Price: 85,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-9.png",
+                },
+                {
+                  id: "oiopijmjkhuihb",
+                  name: "denim pants",
+                  Price: 120,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-10.png",
+                },
+                {
+                  id: "oiopijewyiohbjhib",
+                  name: "basic cap",
+                  Price: 35,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-11.png",
+                },
+                {
+                  id: "rtytytuyuytyytbvncv",
+                  name: "leather boots",
+                  Price: 350,
+                  desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+                  img: "images/img-12.png",
+                },
+              
+        ];
